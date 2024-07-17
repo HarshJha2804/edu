c1590002d82f597d4dfa64ea0ef72c6fa82de27b
Kalpana Bhatt <kalpana@infinites.uk>
2024-07-17 13:15:04 +0530

    --new pages path added + login file created

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,65 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import Home from './component/Home/Home'
 import Layout from './Layout'
 import About from './component/About/about'
 import Services from './component/Services/services'
 import Signup from './component/Signup/Signup'
+import Blog from './component/Home/Blog'
+import Event from './component/Event/Event'
+import Gallery from './component/Gallery/Gallery'
+import Contact from './component/Contact/Contact'
+import Login from './component/Login/login'
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Layout/>,
-    children:[
+    path: '/',
+    element: <Layout />,
+    children: [
       {
-        path:'/',
-        element:<Home/>
+        path: '/',
+        element: <Home />
       },
       {
-        path:'/about',
-        element:<About/>
+        path: '/about',
+        element: <About />
       },
       {
-        path:'/services',
-        element:<Services/>
+        path: '/services',
+        element: <Services />
       },
       {
-        path:'/signup',
-        element:<Signup/>
+        path: '/blog',
+        element: <Blog />
+      },
+      {
+        path: '/event',
+        element: <Event />
+      },
+      {
+        path: '/gallery',
+        element: <Gallery />
+      },
+      {
+        path: '/contact-us',
+        element: <Contact />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
       },
     ]
   },
 ])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
