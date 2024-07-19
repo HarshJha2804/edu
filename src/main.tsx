4eea533d64367863d6fccc1e65bb004a5e219700
Kalpana Bhatt <kalpana@infinites.uk>
2024-07-19 15:24:31 +0530

    page refresh added

--- src/main.tsx
+++ src/main.tsx
@@ -1,65 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './index.css'
-import Home from './component/Home/Home'
-import Layout from './Layout'
-import About from './component/About/about'
-import Services from './component/Services/services'
-import Signup from './component/Signup/Signup'
-import Event from './component/Event/Event'
-import Gallery from './component/Gallery/Gallery'
-import Contact from './component/Contact/Contact'
-import Login from "./component/Login/Login"
-import Blog from './component/Blog/Blog'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
+import './index.css';
+import Home from './component/Home/Home';
+import Layout from './Layout';
+import About from './component/About/about';
+import Services from './component/Services/services';
+import Signup from './component/Signup/Signup';
+import Event from './component/Event/Event';
+import Gallery from './component/Gallery/Gallery';
+import Contact from './component/Contact/Contact';
+import Login from './component/Login/Login';
+import Blog from './component/Blog/Blog';
+
+// ScrollToTop component defined inline
+const ScrollToTop: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+// Layout component that includes ScrollToTop
+const AppLayout: React.FC = () => (
+  <>
+    <ScrollToTop />
+    <Layout />
+  </>
+);
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <AppLayout />, // Use AppLayout which includes ScrollToTop
     children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/about',
-        element: <About />
-      },
-      {
-        path: '/services',
-        element: <Services />
-      },
-      {
-        path: '/blog',
-        element: <Blog />
-      },
-      {
-        path: '/event',
-        element: <Event />
-      },
-      {
-        path: '/gallery',
-        element: <Gallery />
-      },
-      {
-        path: '/contact-us',
-        element: <Contact />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },
-      {
-        path: '/signup',
-        element: <Signup />
-      },
-    ]
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/services', element: <Services /> },
+      { path: '/blog', element: <Blog /> },
+      { path: '/event', element: <Event /> },
+      { path: '/gallery', element: <Gallery /> },
+      { path: '/contact-us', element: <Contact /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
+    ],
   },
-])
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
