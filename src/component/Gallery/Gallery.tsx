e1dac3200d7ec8c1f39211d6cf6783b29496d62b
Kalpana Bhatt <kalpana@infinites.uk>
2024-07-23 18:23:17 +0530

    --gallery changed

--- src/component/Gallery/Gallery.tsx
+++ src/component/Gallery/Gallery.tsx
@@ -8,6 +8,7 @@ import gallery6 from "../../assets/Gallery/gallery-6.jpg"
 import gallery7 from "../../assets/Gallery/gallery-7.jpg"
 import gallery8 from "../../assets/Gallery/gallery-8.jpg"
 import { useEffect, useRef, useState } from "react"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 const Gallery = () => {
   const trackRef = useRef<HTMLDivElement>(null);
@@ -32,9 +33,9 @@ const Gallery = () => {
     const mouseDelta = mouseDownAt - clientX;
     const maxDelta = window.innerWidth / 2;
 
-    const percentageChange = (mouseDelta / maxDelta) * -100;
+    const percentageChange = (mouseDelta / maxDelta) * -120;
     const nextPercentageUnconstrained = prevPercentage + percentageChange;
-    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -120);
 
     setPercentage(nextPercentage);
 
@@ -46,7 +47,7 @@ const Gallery = () => {
 
       Array.from(track.getElementsByClassName('draggable-img')).forEach((image) => {
         (image as HTMLElement).animate({
-          objectPosition: `${100 + nextPercentage}% center`
+          objectPosition: `${120 + nextPercentage}% center`
         }, { duration: 1200, fill: "forwards" });
       });
     }
@@ -83,8 +84,23 @@ const Gallery = () => {
           </div>
 
         </div>
+
         <div className="w-[100vw] h-[100vh] m-0 overflow-hidden bg-black">
+
           <div className="image-track" ref={trackRef}>
+            <div className="draggable-img text-white w-[2000px] mr-[10rem] flex flex-col justify-between"  >
+              <h2 className="text-[7rem] leading-[6rem]">
+                {/* Empowering, */}
+                Meet Our Team
+              </h2>
+              <div className="tooltip-dark">
+                <span className="tooltiptext-dark"> Drag to right</span>
+                <div className="p-6 border-[1px] border-white w-[fit-content] rounded-full">
+                  <Icon icon="ci:arrow-up-md" className="text-white rotate-90" fontSize={32} />
+                </div>
+              </div>
+
+            </div>
             <img src={gallery1} alt="" draggable="false" className="draggable-img" />
             <img src={gallery2} alt="" draggable="false" className="draggable-img" />
             <img src={gallery3} alt="" draggable="false" className="draggable-img" />
