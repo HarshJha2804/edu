0aaaaf3d70a5b5cd8cbc3d1be37b15894cedc470
Kalpana Bhatt <kalpana@infinites.uk>
2024-08-01 18:11:15 +0530

    thumbnail changed

--- src/component/Home/Youtubeevideo.tsx
+++ src/component/Home/Youtubeevideo.tsx
@@ -2,7 +2,7 @@
 
 // import { useState } from "react"
 import { useState } from "react";
-import thumbnail from "../../assets/Homepage/Herosection/herosection.png"
+import thumbnail from "../../assets/Homepage/Youtubevideo/new-thumbnail.jpg"
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Youtubeevideo = () => {
@@ -12,15 +12,17 @@ const Youtubeevideo = () => {
 
   return (
     <>
-      <div className=" rounded-2xl hover:cursor-pointer" style={{ backgroundImage: `url(${thumbnail})`, backgroundSize: 'cover' }} onClick={openModal} >
-
+      <div className=" rounded-2xl hover:cursor-pointer flex justify-center items-center" style={{ backgroundImage: `url(${thumbnail})`, backgroundSize: 'cover', }} >
+        <button onClick={openModal} className="p-6 bg-primary rounded-[100%]" >
+          <Icon icon="ph:play-fill" fontSize={24} style={{ color: 'white' }} />
+        </button>
       </div >
       {isModalOpen &&
         <>
           <div className="w-[100vw] h-[100vh] bg-black fixed z-1000000 top-0 right-0 left-0 flex justify-center items-center">
             <div className="relative w-[80vh]">
               <button className="absolute close-btn text-white right-[-2rem] top-[-2rem]" onClick={closeModal} >
-                <Icon icon="mingcute:close-line" fontSize={20} />
+                <Icon icon="mingcute:close-line" fontSize={24} />
               </button>
 
               <div className="w-[100%] flex justify-center items-center">
