6c14c0136c77e978c02b3ef5f49ab18b19abe7e9
Kalpana Bhatt <kalpana@infinites.uk>
2024-08-10 15:55:04 +0530

    --changed the thumbnail image

--- src/component/Home/Youtubeevideo.tsx
+++ src/component/Home/Youtubeevideo.tsx
@@ -2,7 +2,8 @@
 
 // import { useState } from "react"
 import { useState } from "react";
-import thumbnail from "../../assets/Homepage/Youtubevideo/videoThumbnail.jpeg"
+
+import ReduceWidth from "../../assets/Homepage/Youtubevideo/lessWidth.jpeg"
 import { Icon } from "@iconify/react/dist/iconify.js";
 // import VideoModal from "./videomodal";
 
@@ -11,13 +12,49 @@ const Youtubeevideo = () => {
   const openModal = () => setModalOpen(true)
   const closeModal = () => setModalOpen(false)
 
+  // const [backgroundImage, setBackgroundImage] = useState(ReduceWidth);
+
+  // useEffect(() => {
+  //   const divElement = document.querySelector('.my-div');
+
+  //   const resizeObserver = new ResizeObserver((entries) => {
+  //     for (let entry of entries) {
+  //       const width = entry.contentRect.width;
+  //       if (width > 500) {
+  //         setBackgroundImage(thumbnail);
+  //       } else {
+  //         setBackgroundImage(ReduceWidth);
+  //       }
+  //     }
+  //   });
+
+  //   if (divElement) {
+  //     resizeObserver.observe(divElement);
+  //   }
+
+  //   return () => {
+  //     if (divElement) {
+  //       resizeObserver.unobserve(divElement);
+  //     }
+  //   };
+  // }, [ReduceWidth, thumbnail]);
+
   return (
     <>
-      <div className=" rounded-2xl hover:cursor-pointer flex justify-center items-center" style={{ backgroundImage: `url(${thumbnail})`, backgroundSize: 'cover', height: '40vh', backgroundPosition: 'center' }} >
-        <button onClick={openModal} className="p-6 bg-[#FF0000] rounded-[100%]" >
+      <div
+        className="rounded-2xl hover:cursor-pointer flex justify-center items-center"
+        style={{
+          backgroundImage: `url(${ReduceWidth})`,
+          backgroundSize: 'cover',
+          height: '40vh',
+          backgroundPosition: 'center',
+        }}
+      >
+        <button onClick={openModal} className="p-6 bg-[#FF0000] rounded-full">
           <Icon icon="ph:play-fill" fontSize={24} style={{ color: 'white' }} />
         </button>
-      </div >
+      </div>
+
       {isModalOpen &&
         <>
           <div className="w-[100vw] h-[100vh] z-[100000000000] bg-black fixed top-0 right-0 left-0 flex justify-center items-center">
@@ -25,6 +62,8 @@ const Youtubeevideo = () => {
               <button className="absolute close-btn text-white right-[-0.75rem] top-[-0.75rem]" onClick={closeModal} >
                 <Icon icon="mingcute:close-line" fontSize={24} />
               </button>
+
+
 
               <div className="w-[100%] flex justify-center items-center h-[60vh] z-[100000000000]">
                 {/* <VideoModal /> */}
